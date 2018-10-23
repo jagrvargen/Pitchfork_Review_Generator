6590c614e6ed5510ffb31fe04a6cc92182d67e58
Jesse Hedden <jagarvargen@Jesses-MacBook-Pro.local>
2018-10-22 20:49:16 -0700

    added function to clean/tokenize text

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 """ Helper functions for text processing. """
 
+import nltk.tokenize.punkt as punkt
 import re
 import string
 
@@ -9,12 +10,14 @@ def get_raw_text():
        Retrieve text data and remove most punctuation and irrelevant
        SQL syntax.
     """
+
     with open('data/reviews_corpus.txt', 'r', encoding='utf-8') as f:
         text = f.read()
 
     regex = re.compile(u"INSERT INTO content VALUES\(\d+,\'|\'\);|[^a-zA-Z_.0-9]")
     text = re.sub(regex, ' ', text)
 
+    """ Save cleaned parsed text for later use. """
     with open('data/parsed_text.txt', 'w+', encoding='utf-8') as f:
         f.write(text)
 
@@ -24,3 +27,8 @@ def clean_and_tokenize_text(text):
     """
        Convert all text to lowercase and tokenize at the sentence level.
     """
+
+    tokenizer = punkt.PunktSentenceTokenizer()
+    cleaned_text = tokenizer.tokenize(text.lower())
+
+    return cleaned_text
